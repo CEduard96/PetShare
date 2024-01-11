3b2afb1fcbb7c29e8fe5a55ae44b8c58ec8394f5
CreangaEduard <creanga.eduard1996@gmail.com>
2024-01-11 23:04:20 +0200

    First commit

---
+++ src/test/java/com/creanga/petshare/PetshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creanga.petshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
